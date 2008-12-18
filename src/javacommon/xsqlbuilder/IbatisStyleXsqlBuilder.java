8a551de1445a1ee1487aa2bf10596369f9cca9ef
badqiu <badqiu@8572161f-5e4f-0410-be01-df31a61b1245>
2008-12-18 16:17:23 +0000

    增加注释

--- src/javacommon/xsqlbuilder/IbatisStyleXsqlBuilder.java
+++ src/javacommon/xsqlbuilder/IbatisStyleXsqlBuilder.java
@@ -1,5 +1,15 @@
 package javacommon.xsqlbuilder;
 
+/**
+ * 支持Ibatis类似的语法
+ * <pre>
+ * 		String sql = "select * from user where 1=1"
+ *			+"/~ and username = #username#~/"
+ *			+"/~ and pwd = '$password$'~/";
+ * </pre>
+ * @author badqiu
+ *
+ */
 public class IbatisStyleXsqlBuilder extends XsqlBuilder{
 	
 	public IbatisStyleXsqlBuilder() {
