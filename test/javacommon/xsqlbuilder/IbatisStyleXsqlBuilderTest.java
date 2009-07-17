603d9c4ba302263fc19646922dd246b05f88be56
badqiu <badqiu@8572161f-5e4f-0410-be01-df31a61b1245>
2009-07-17 09:30:00 +0000

    Initial

--- test/javacommon/xsqlbuilder/IbatisStyleXsqlBuilderTest.java
+++ test/javacommon/xsqlbuilder/IbatisStyleXsqlBuilderTest.java
@@ -50,4 +50,19 @@ public class IbatisStyleXsqlBuilderTest extends TestCase {
 		assertEquals(1,result.getAcceptedFilters().size());
 		assertTrue(result.getAcceptedFilters().keySet().contains("username"));
 	}
+	
+	public void testGenerateHql1() {
+		Map filters = new HashMap();
+		String sql = "select * from user where 1=1/~ and username = #username# and username<>#username#~//~ and pwd = #password#~//~ and age = #age#~/";
+		String result = builder.generateHql(sql, filters).getXsql().toString();
+		assertEquals("select * from user where 1=1",result);
+		
+		filters.put("username", "badqiu");
+		filters.put("age", "12");
+		result = builder.generateHql(sql, filters).getXsql().toString();
+		assertEquals("select * from user where 1=1 and username = :username and username<>:username and age = :age",result);
+		
+		result = builder.generateHql("select * from user /~where password=#password#~/ order by username desc", filters).getXsql().toString();
+		assertEquals("select * from user  order by username desc",result);
+	}
 }
